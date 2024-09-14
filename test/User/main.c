b2d43319804c01a837c87d2a5de37295b7ea24f6
Tyler0o0 <heibaimeiyouda@gmail.com>
2024-09-14 12:08:08 +0800

    9_14

--- test/User/main.c
+++ test/User/main.c
@@ -10,16 +10,16 @@ int main(void)
 //	GPIOC->ODR = 0x00002000;
 	
 	//库函数实现PC13控制
-//	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE);
-//	GPIO_InitTypeDef GPIO_InitStructure;
-//	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-//	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
-//	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-//	GPIO_Init(GPIOC,&GPIO_InitStructure);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE);
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOC,&GPIO_InitStructure);
 //	GPIO_SetBits(GPIOC,GPIO_Pin_13);//高电平
-//	GPIO_ResetBits(GPIOC,GPIO_Pin_13);
+	GPIO_ResetBits(GPIOC,GPIO_Pin_13);
 	while(1)
 	{
 		
-	}
+	}//
 }
